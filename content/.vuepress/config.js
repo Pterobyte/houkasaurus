dae5dc2a3bcd331f30df3ef62d4a07b9422fc9c6
Pterobyte <jt1992@gmail.com>
2019-06-09 14:15:40 +0800

    add: fetched content

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -1,25 +1,43 @@
 module.exports = {
-  title: 'JT Houk',
-  description: ' ',
-  themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Projects', link: '/projects/' },
-        { text: 'Blog', link: '/blog/' },
-      ],
-      sidebar: {
-          '/projects/': [
-              '',
-              'akkadu',
-              'dropbox-paper-cms'
-          ],
-          '/blog/': [
-              '',
-          ]
+  "title": "JT Houk",
+  "description": " ",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/"
+      },
+      {
+        "text": "Projects",
+        "link": "/projects/"
+      },
+      {
+        "text": "Blog",
+        "link": "/blog/"
       }
+    ],
+    "sidebar": {
+      "/projects/": [
+        "",
+        "akkadu",
+        "dropbox-paper-cms"
+      ],
+      "/blog/": [
+        "",
+        "how-to-create-an-api-reference-for-your-node-app-with-swagger",
+        "how-to-publish-a-half-decent-npm-module",
+        "how-to-use-dropbox-paper-as-a-headless-cms"
+      ]
+    }
   },
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ]
   ],
-  evergreen: true,
+  "evergreen": true
 }
---
+++ content/blog/how-to-publish-a-half-decent-npm-module.md
@@ -0,0 +1,16 @@
+---
+{
+  "doc_id": "RlnquU0iOCdMziSpl0Hht",
+  "owner": "jt.houk@outlook.com",
+  "title": "How to Publish a Half Decent NPM Module",
+  "created_date": "2019-06-08T09:02:52Z",
+  "status": {
+    ".tag": "active"
+  },
+  "revision": 15,
+  "last_updated_date": "2019-06-08T09:02:56Z",
+  "last_editor": "jt.houk@outlook.com"
+}
+---
+# How to Publish a Half Decent NPM Module
+
