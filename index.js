fe92c938fb72d8cd08e2e59a2f91195477d4d8a7
HoukasaurusRex <jt@houk.space>
2021-01-05 17:29:42 +0800

    fix: update fetch to markdown

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv-defaults').config()
+const path = require('path')
 const { fetchToMarkdown } = require('fetch-to-markdown')
 
 const contentAPI = process.env.CMS_API
+const contentDir = path.join(`${__dirname}/content`)
 
 const components = {
   Comments: '<Comments />',
@@ -17,14 +19,20 @@ const fetchAllContent = () =>
   Promise.all([
     fetchToMarkdown(contentAPI, 'articles', {
       components: articleComponents,
+      contentDir: `${contentDir}/articles`,
     }),
     fetchToMarkdown(contentAPI, 'projects', {
       readme: `---\nsidebar: false\n---\n${components.Cards}`,
+      contentDir: `${contentDir}/projects`,
     }),
-    fetchToMarkdown(contentAPI, 'companies'),
-    fetchToMarkdown(contentAPI, 'about'),
+    fetchToMarkdown(contentAPI, 'companies', {
+      contentDir: `${contentDir}/companies`,
+    }),
+    fetchToMarkdown(contentAPI, 'links', { contentDir: `${contentDir}/links` }),
+    fetchToMarkdown(contentAPI, 'about', { contentDir: `${contentDir}/about` }),
     fetchToMarkdown(contentAPI, 'landing', {
       components: landingComponents,
+      contentDir: `${contentDir}/`,
     }),
   ])
 
